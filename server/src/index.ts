d8d72011ca82efe15e23d1e851f0fe9d83ffa841
f0rsakeN-afk <nar3sh0@gmail.com>
2025-07-17 22:54:34 +0545

    Added error middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middlewares/error.middleware";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(errorMiddleware);
 
 export default app;
--- server/src/middlewares/error.middleware.ts
+++ server/src/middlewares/error.middleware.ts
@@ -1,1 +1,18 @@
-import{} from "express"
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../utils/AppError";
+
+export const errorMiddleware = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statusCode = err.statusCode || 500;
+  const message =
+    err instanceof AppError ? err.message : "Something went wrong";
+
+  return res.status(statusCode).json({
+    success: false,
+    message,
+  });
+};

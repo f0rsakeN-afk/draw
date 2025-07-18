d89ad0215ca802c493f18a334a0ff56ade2f3996
f0rsakeN-afk <nar3sh0@gmail.com>
2025-07-18 09:27:36 +0545

    Added schema for signup

---
+++ client/src/schema/auth/signup/signup.schema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const registerSchema = z
+  .object({
+    username: z.string().min(2, "Username must contain at least 2 characters"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must contain at least 6 characters"),
+    passwordConfirm: z
+      .string()
+      .min(6, "Password confirm must contain at least 6 characters"),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    path: ["passwordConfirm"],
+    message: "Passwords do not match",
+  });
+
+export type registerSchemaData = z.infer<typeof registerSchema>;

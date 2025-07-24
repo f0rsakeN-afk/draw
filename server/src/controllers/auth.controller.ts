b2a4091e3ccc21a2692121086ffc693eeb5e616d
f0rsakeN-afk <naresh0@gmail.com>
2025-07-24 08:31:07 +0545

    updated auth controller

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import * as AuthService from "../services/authService";
 import { catchAsync } from "../utils/catchAsync";
 
@@ -21,14 +21,40 @@ export const login = catchAsync(async (req: Request, res: Response) => {
 
   const token = await AuthService.loginUser(email, password);
 
-  res.cookie("token", token, {
+  res.cookie("vizion", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "strict",
-    maxAge: Number(process.env.COOKIE_EXPIRES_IN),
+    maxAge: 89 * 24 * 60 * 60 * 1000,
   });
 
   res.status(200).json({
     message: "Logged in successfully",
+  });
+});
+
+export const resendOTP = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const email = req.body;
+      const result = await AuthService.resendOTPService(email);
+      res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+export const logout = catchAsync(async (req: Request, res: Response) => {
+  res.clearCookie("vizion", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  });
+
+  const result = AuthService.logoutUser;
+  res.status(200).json({
+    success: true,
+    ...result,
   });
 });

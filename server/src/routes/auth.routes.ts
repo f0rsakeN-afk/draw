da5ab0219dcd998d522edf58706d479053416168
f0rsakeN-afk <nar3sh0@gmail.com>
2025-07-25 10:53:46 +0545

    added auth routes

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -6,13 +6,21 @@ import {
   login,
   logout,
   resendOTP,
+  forgotPassword,
+  resetPassword,
+  updatePassword,
+  getMe,
 } from "../controllers/auth.controller";
 import {
   registerSchema,
   loginSchema,
   verifyOTPSchema,
   resendOTPSchema,
+  forgotPasswordSchema,
+  resetPasswordSchema,
+  updatePasswordSchema,
 } from "../schemas/auth.schema";
+import { protect } from "../middlewares/auth.middleware";
 
 const router = Router();
 
@@ -20,7 +28,26 @@ router.post("/register", validateRequest(registerSchema), register);
 router.post("/verifyOTP", validateRequest(verifyOTPSchema), verifyOTP);
 router.post("/login", validateRequest(loginSchema), login);
 router.post("/logout", logout);
-
 router.post("/resendOTP", validateRequest(resendOTPSchema), resendOTP);
+router.post(
+  "/forgotpassword",
+  validateRequest(forgotPasswordSchema),
+  forgotPassword
+);
+router.post(
+  "/resetpassword",
+  validateRequest(resetPasswordSchema),
+  resetPassword
+);
+
+router.use(protect);
+
+router.post(
+  "updatepassword",
+  validateRequest(updatePasswordSchema),
+  updatePassword
+);
+
+router.get("/me", getMe);
 
 export default router;

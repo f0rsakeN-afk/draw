209cc97fc59ff3753b6a1dbb59740c7617faa1cf
f0rsakeN-afk <nar3sh0@gmail.com>
2025-07-25 11:50:42 +0545

    Added user role

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -4,6 +4,8 @@ import { AppError } from "../utils/AppError";
 import { catchAsync } from "../utils/catchAsync";
 import { verifyToken } from "../utils/jwt";
 
+type UserRole = 'user' | 'admin' | 'superadmin';
+
 export const protect = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const token = req.cookies?.vizion;
@@ -21,3 +23,27 @@ export const protect = catchAsync(
     next();
   }
 );
+
+/**
+ * Restrict access to specific roles
+ * @param roles - Array of roles that are allowed to access the route
+ * @returns Middleware function
+ */
+export const restrictTo = (...roles: UserRole[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    // req.user is set by the protect middleware
+    if (!req.user) {
+      return next(
+        new AppError('You are not logged in! Please log in to get access.', 401)
+      );
+    }
+
+    if (!roles.includes(req.user.role as UserRole)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
+    }
+
+    next();
+  };
+};
